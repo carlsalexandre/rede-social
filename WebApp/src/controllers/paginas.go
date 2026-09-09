de558b21f684e6731f9c0742aad1f580576b156f
carlsalexandre <contatocarlosalexandrecb@gmail.com>
2026-09-09 18:15:52 -0300

    feat: add Publicacao model and authentication request function
    - Created a new Publicacao model with fields for ID, title, content, author ID, author nickname, likes, and creation time.
    - Implemented RequisicaoComAutenticacao function to handle authenticated HTTP requests, including token management via cookies.

--- WebApp/src/controllers/paginas.go
+++ WebApp/src/controllers/paginas.go
@@ -1,7 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/config"
+	"webapp/src/models"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -14,5 +20,24 @@ func CarregarCadastro(w http.ResponseWriter, r *http.Request) {
 }
 
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
-	utils.ExecutarTemplate(w, "home.html", nil)
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	response, erro := requisicoes.RequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroDaAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, response)
+		return
+	}
+
+	var publicacoes []models.Publicacao
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroDaAPI{Erro: erro.Error()})
+		return
+	}
+
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
--- WebApp/src/middlewares/middlewares.go
+++ WebApp/src/middlewares/middlewares.go
@@ -17,8 +17,8 @@ func Autenticar(proximaFuncao http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if _, erro := cookies.Ler(r); erro != nil {
 			http.Redirect(w, r, "/login", 302)
-			return 
-		}		
+			return
+		}
 		proximaFuncao(w, r)
 	}
 }
